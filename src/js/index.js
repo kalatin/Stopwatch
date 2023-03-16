2f57c2a28fb71f62067e720bf1771986e29cd4b3
Ruslan Pitsik <rs.pitsik@gmail.com>
2023-03-16 19:15:54 +0300

    add work timer

---
+++ src/js/index.js
@@ -0,0 +1,83 @@
+let laps = [];
+
+class Timer {
+	constructor(secondsElem, minutesElem, hoursElem) {
+		this.milliseconds = 0;
+		this.secondsElem = secondsElem;
+		this.minutesElem = minutesElem;
+		this.hoursElem = hoursElem;
+		this.interval;
+	}
+
+	startTimer() {
+		this.milliseconds = this.milliseconds + 1;
+		if (this.milliseconds > 99) {
+			this.milliseconds = 0;
+			this.secondsElem.textContent = String(+this.secondsElem.textContent + 1).padStart(2, '0');
+		}
+		if (+this.secondsElem.textContent > 59) {
+			this.secondsElem.textContent = '00';
+			this.minutesElem.textContent = String(+this.minutesElem.textContent + 1).padStart(2, '0');
+		} else if (+this.minutesElem.textContent > 59) {
+			this.minutesElem.textContent = '00';
+			this.hoursElem.textContent = String(+this.hoursElem.textContent + 1).padStart(2, '0');
+		}
+	}
+
+	clearFields() {
+		this.milliseconds = 0;
+		this.secondsElem.textContent = '00';
+		this.minutesElem.textContent = '00';
+		this.hoursElem.textContent = '00';
+	}
+}
+
+let ex1Timer = new Timer(
+	document.querySelector('.timer__seconds'),
+	document.querySelector('.timer__minutes'),
+	document.querySelector('.timer__hours')
+);
+
+document.addEventListener('click', e => {
+	if (e.target.closest('.start')) {
+		clearInterval(ex1Timer.interval);
+		ex1Timer.interval = setInterval(() => {
+			ex1Timer.startTimer();
+		}, 10);
+		e.target.classList.remove('start');
+		e.target.classList.add('pause');
+		e.target.textContent = 'Стоп';
+	} else if (e.target.closest('.pause')) {
+		clearInterval(ex1Timer.interval);
+		e.target.classList.remove('pause');
+		e.target.classList.add('start');
+		e.target.textContent = 'Старт';
+	} else if (e.target.closest('.reset')) {
+		clearInterval(ex1Timer.interval);
+		ex1Timer.clearFields();
+		if (document.querySelector('.pause')) {
+			document.querySelector('.pause').textContent = 'Старт';
+			document.querySelector('.pause').classList.add('start');
+			document.querySelector('.pause').classList.remove('pause');
+		}
+		document.querySelectorAll('.timer__lap').forEach(item => {
+			item.remove();
+		});
+	} else if (e.target.closest('.lap')) {
+		if (document.querySelector('.pause')) {
+			let hours = ex1Timer.hoursElem.textContent;
+			let minutes = ex1Timer.minutesElem.textContent;
+			let seconds = ex1Timer.secondsElem.textContent;
+
+			document.querySelector('.timer__laps').insertAdjacentHTML(
+				'beforeend',
+				`
+				<div class="timer__lap">
+					<div class="timer__lap-num">${String(document.querySelector('.timer__laps').childElementCount).padStart(2, '0')}</div>
+					<div class="timer__lap-time">${hours}:${minutes}:${seconds}</div>
+				</div>
+			`
+			);
+		}
+	}
+});
